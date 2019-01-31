dc851f55120401875b5822506cc6ca1f572c530a
gsi-chao <chao@generalsoftwareinc.com>
2019-01-31 12:22:40 -0500

    Add Sortable Function to Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { VTable } from "./components/Table/Table";
 
@@ -7,27 +6,37 @@ class App extends Component {
   render() {
     const data = [
       { name: "Carlos", lastname: "Chao" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" },
-      { name: "Leticia", lastname: "Larrosa" }
+      { name: "Name1", lastname: "Lastname1" },
+      { name: "Name2", lastname: "Lastname2" },
+      { name: "Name3", lastname: "Lastname3" },
+      { name: "Name4", lastname: "Lastname4" },
+      { name: "Name5", lastname: "Lastname5" },
+      { name: "Name6", lastname: "Lastname6" },
+      { name: "Name7", lastname: "Lastname7" },
+      { name: "Name7", lastname: "Lastname7" },
+      { name: "Name7", lastname: "Lastname7" },
+      { name: "Name7", lastname: "Lastname7" },
+      { name: "Name7", lastname: "Lastname7" },
     ];
+
+    // validator example
+    const nameValidation = (value: string) => {
+      return value.length > 5;
+    };
+
     return (
       <VTable
-        edit={{ columns: ["name"] }}
-        columns={["name", "lastname"]}
-        data={data}
+          edit={{ columns: ["name"], validation: {name: nameValidation} }}
+          columns={["name", "lastname"]}
+          sortable={{columns: ["name"], onSort: this.onSort}}
+          data={data}
       />
     );
+  }
+
+  onSort = (index: number, order: string) => {
+    console.log(index);
+    console.log(order);
   }
 }
 
---
+++ src/components/Table/TableColumn.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { IVActionSortableTableProps } from "./Table";
+import { Cell, Column, ColumnHeaderCell } from "@blueprintjs/table";
+import { Menu, MenuItem } from "@blueprintjs/core";
+
+export type ICellLookup = (rowIndex: number, columnIndex: number) => any;
+
+export interface ISortableColumn {
+  getColumn(getCellData: ICellLookup): JSX.Element;
+}
+
+export default class TableColumn implements ISortableColumn {
+  constructor(
+    protected name: string,
+    protected index: number,
+    protected columns: string[],
+    protected sortable?: IVActionSortableTableProps
+  ) {}
+
+  getColumn = (getCellData: ICellLookup) => {
+    return (
+      <Column
+        cellRenderer={getCellData}
+        columnHeaderCellRenderer={this.renderColumnHeader}
+        key={this.index}
+        name={this.name}
+      />
+    );
+  };
+
+  renderMenu = (index?: number) => {
+    const sortAsc = () => this.sortable!.onSort!(index!, "ASC");
+    const sortDesc = () => this.sortable!.onSort!(index!, "DESC");
+    return (
+      <Menu>
+        <MenuItem icon="sort-asc" onClick={sortAsc} text="Sort Rank Asc" />
+        <MenuItem icon="sort-desc" onClick={sortDesc} text="Sort Rank Desc" />
+      </Menu>
+    );
+  };
+
+  renderColumnHeader = (columnIndex: number) => {
+    let menuRenderer = null;
+    if (
+      this.sortable &&
+      this.sortable.columns.indexOf(this.columns[columnIndex]) != -1
+    ) {
+      if (
+        this.sortable.custom_render_menu &&
+        this.sortable.custom_render_menu.indexOf(this.columns[columnIndex])
+      ) {
+        menuRenderer = this.sortable.custom_render_menu[
+          this.columns[columnIndex]
+        ];
+      } else {
+        menuRenderer = this.renderMenu;
+      }
+    }
+    return (
+      <ColumnHeaderCell
+        name={this.columns[columnIndex].replace(/\b\w/g, l => l.toUpperCase())}
+        menuRenderer={menuRenderer as any}
+      />
+    );
+  };
+}
