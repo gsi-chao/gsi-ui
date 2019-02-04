8face0a1d92034e35d4f5f5fd0ac9292442be2d4
gsi-chao <chao@generalsoftwareinc.com>
2019-02-04 12:46:46 -0500

    Fix error in reordering feature prop.

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -31,7 +31,7 @@ export interface IVTableProps {
   contextual?: IVContextualTableProps;
   columns: string[];
   data: any;
-  reordering:boolean;
+  reordering?:boolean;
 }
 
 interface IProps extends IVTableProps, ITableProps {}
