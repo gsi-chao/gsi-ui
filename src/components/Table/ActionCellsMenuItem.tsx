284cdebef4e9046f15f8baebdcae20446599bc6f
gsi-arturo <arturo@generalsofwareinc.com>
2019-02-13 17:09:25 -0500

    ADDED: Added support for copy paste cells

--- src/components/Table/ActionCellsMenuItem.tsx
+++ src/components/Table/ActionCellsMenuItem.tsx
@@ -6,108 +6,165 @@ import { Clipboard, IMenuContext, Regions } from '@blueprintjs/table';
 export type DefaultActions = 'copy' | 'paste' | 'export';
 
 export interface IVContextualActionTableProps {
-  action: (item: any) => void;
-  text: string;
-  icon: IconName;
+    action: (item: any) => void;
+    text: string;
+    icon: IconName;
+}
+
+export interface ICell {
+    col: number;
+    row: number;
 }
 
 export interface IVContextualTableProps {
-  columns: string[];
-  actions?: IVContextualActionTableProps[];
-  default_actions: DefaultActions[];
+    columns: string[];
+    actions?: IVContextualActionTableProps[];
+    default_actions: DefaultActions[];
 }
 
 export interface IActionCellMenuItemProps {
-  /**
-   * The `IMenuContext` that launched the menu.
-   */
-  context: IMenuContext;
+    /**
+     * The `IMenuContext` that launched the menu.
+     */
+    context: IMenuContext;
 
-  /**
-   * A callback that returns the data for a specific cell. This need not
-   * match the value displayed in the `<Cell>` component. The value will be
-   * invisibly added as `textContent` into the DOM before copying.
-   */
-  getCellData: (row: number, col: number) => any;
+    /**
+     * A callback that returns the data for a specific cell. This need not
+     * match the value displayed in the `<Cell>` component. The value will be
+     * invisibly added as `textContent` into the DOM before copying.
+     */
+    getCellData: (row: number, col: number) => any;
 
-  context_options: IVContextualTableProps;
+    context_options: IVContextualTableProps;
 
-  onDefaultActions: (action: DefaultActions, value: any) => void;
+    onDefaultActions: (action: DefaultActions, value: any) => void;
+    setCachedData: any;
+    cachedData: any;
+    setStateData: any;
 }
 
 export class ActionCellsMenuItem extends React.PureComponent<
-  IActionCellMenuItemProps
-> {
-  render() {
-    const default_items = this.renderDefaultMenuItems();
-    const actions = this.renderActionsMenuItems();
-    return (
-      <Menu>
-        {default_items}
-        {actions}
-      </Menu>
-    );
-  }
-
-  renderDefaultMenuItems = () => {
-    if (
-      this.props.context_options &&
-      this.props.context_options.default_actions
-    ) {
-      const default_actions = this.props.context_options.default_actions;
-      return default_actions.map((value: string, key: number) => {
-        switch (value) {
-          case 'copy':
-            return (
-              <MenuItem
-                key={key}
-                icon="style"
-                text="Copy"
-                onClick={this.handleCopy}
-              />
-            );
-          case 'paste':
-            return (
-              <MenuItem
-                key={key}
-                icon="cut"
-                text="Paste"
-                onClick={this.handlePaste}
-              />
-            );
-          case 'export':
-            return <MenuItem key={key} icon="export" text="Export" />;
-        }
-      });
-    }
-  };
-
-  renderActionsMenuItems = () => {
-    if (this.props.context_options && this.props.context_options.actions) {
-      const actions = this.props.context_options.actions;
-      return actions.map((value: IVContextualActionTableProps, key: number) => {
+    IActionCellMenuItemProps
+    > {
+    render() {
+        const default_items = this.renderDefaultMenuItems();
+        const actions = this.renderActionsMenuItems();
         return (
-          <MenuItem
-            key={key}
-            icon={value.icon}
-            text={value.text}
-            onClick={value.action}
-          />
+            <Menu>
+                {default_items}
+                {actions}
+            </Menu>
         );
-      });
     }
-  };
 
-  private handleCopy = () => {
-    const { context, getCellData } = this.props;
-    const cells = context.getUniqueCells();
-    const sparse = Regions.sparseMapCells(cells, getCellData);
-    const success = Clipboard.copyCells(sparse);
-    console.log(sparse);
-    console.log(context);
-  };
+    renderDefaultMenuItems = () => {
+        if (
+            this.props.context_options &&
+            this.props.context_options.default_actions
+        ) {
+            const default_actions = this.props.context_options.default_actions;
+            return default_actions.map((value: string, key: number) => {
+                switch (value) {
+                    case 'copy':
+                        return (
+                            <MenuItem
+                                key={key}
+                                icon="style"
+                                text="Copy"
+                                onClick={this.handleCopy}
+                            />
+                        );
+                    case 'paste':
+                        return (
+                            <MenuItem
+                                key={key}
+                                icon="cut"
+                                text="Paste"
+                                onClick={this.handlePaste}
+                            />
+                        );
+                    case 'export':
+                        return <MenuItem key={key} icon="export" text="Export" />;
+                }
+            });
+        }
+    };
 
-  private handlePaste = () => {
-    const { context, getCellData } = this.props;
-  };
+    renderActionsMenuItems = () => {
+        if (this.props.context_options && this.props.context_options.actions) {
+            const actions = this.props.context_options.actions;
+            return actions.map((value: IVContextualActionTableProps, key: number) => {
+                return (
+                    <MenuItem
+                        key={key}
+                        icon={value.icon}
+                        text={value.text}
+                        onClick={value.action}
+                    />
+                );
+            });
+        }
+    };
+
+    private handleCopy = () => {
+        const {context} = this.props;
+        this.getDataToCopy(context);
+    };
+
+    private handlePaste = () => {
+        const {context} = this.props;
+        const cells = context.getUniqueCells();
+        const tempPivotCell: number[] = cells[0];
+        const pivotCell: ICell = {
+            col: tempPivotCell[1],
+            row: tempPivotCell[0]
+        };
+        const cachedData = this.props.cachedData();
+        if (cachedData) {
+            cachedData.map( (cellData: any) => {
+                const {value, colFromPivot, rowFromPivot} = cellData;
+                const col = colFromPivot + pivotCell.col;
+                const row = rowFromPivot + pivotCell.row;
+                this.props.setStateData(row, col, value);
+            });
+            this.props.setCachedData(null);
+        }
+    };
+
+    getDataToCopy = (context: IMenuContext) => {
+        const regions = context.getRegions();
+        const firstPivotCell: ICell = {
+            col: regions[0].cols && regions[0].cols[0] || 0,
+            row: regions[0].rows && regions[0].rows[0] || 0
+        };
+        const cellsArray: any[] = [];
+        regions.map(region => {
+            const startCell: ICell = {
+                col: region.cols && region.cols[0] || 0,
+                row: region.rows && region.rows[0] || 0
+            };
+            const endCell: ICell = {
+                col: region.cols && region.cols[1] || 0,
+                row: region.rows && region.rows[1] || 0
+            };
+            let rowFromPivot = startCell.row - firstPivotCell.row;
+            let colFromPivot = startCell.col - firstPivotCell.col;
+            for (let index = startCell.col; index <= endCell.col; index++) {
+                for (let indexY = startCell.row; indexY <= endCell.row; indexY++) {
+                    const value = this.props.getCellData(indexY, index);
+                    cellsArray.push(this.createCellForCopy(colFromPivot, rowFromPivot, value));
+                    rowFromPivot++;
+                }
+                rowFromPivot = 0;
+                colFromPivot++;
+            }
+        });
+        this.props.setCachedData(cellsArray);
+    };
+
+    createCellForCopy = (colFromPivot: number, rowFromPivot: number, value: any) => {
+        return {
+            colFromPivot, rowFromPivot, value
+        }
+    }
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,240 +1,254 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Cell,
-  EditableCell,
-  IMenuContext,
-  ITableProps,
-  Table,
-  Utils
+    Cell,
+    EditableCell,
+    IMenuContext,
+    ITableProps,
+    Table,
+    Utils
 } from '@blueprintjs/table';
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/table/lib/css/table.css';
-import { IconName, Intent } from '@blueprintjs/core';
+import {IconName, Intent} from '@blueprintjs/core';
 import TableColumn from './TableColumn';
 import {
-  ActionCellsMenuItem,
-  DefaultActions,
-  IVContextualTableProps
+    ActionCellsMenuItem,
+    DefaultActions,
+    IVContextualTableProps
 } from './ActionCellsMenuItem';
 
 export type IVTableOrder = 'ASC' | 'DESC';
 
 export interface IVActionsTableProps {
-  columns: string[];
+    columns: string[];
 }
 
 export interface IVActionEditTableProps extends IVActionsTableProps {
-  validation?:  { [key: string]: (value: string) => boolean };
+    validation?: { [key: string]: (value: string) => boolean };
 }
 
 export interface IVCustomActionSortableTableProp {
-  name: string;
-  text: string;
-  icon: IconName;
-  callback: (value: any) => void;
+    name: string;
+    text: string;
+    icon: IconName;
+    callback: (value: any) => void;
 }
 
 export interface IVActionSortableTableProps extends IVActionsTableProps {
-  custom_render_menu?: { [key: string]: IVCustomActionSortableTableProp };
-  onSort?: (columnIndex: number, order: IVTableOrder) => void;
+    custom_render_menu?: { [key: string]: IVCustomActionSortableTableProp };
+    onSort?: (columnIndex: number, order: IVTableOrder) => void;
 }
 
 export interface IVTableProps {
-  edit?: IVActionEditTableProps;
-  search?: IVActionsTableProps;
-  sortable?: IVActionSortableTableProps;
-  contextual?: IVContextualTableProps;
-  columns: string[];
-  data: any;
-  reordering?: boolean;
+    edit?: IVActionEditTableProps;
+    search?: IVActionsTableProps;
+    sortable?: IVActionSortableTableProps;
+    contextual?: IVContextualTableProps;
+    columns: string[];
+    data: any;
+    reordering?: boolean;
 }
 
-interface IProps extends IVTableProps, ITableProps {}
+interface IProps extends IVTableProps, ITableProps {
+}
 
 export interface IVTableState {
-  sparseCellData: any[];
-  sparseCellInvalid?: { [key: string]: Intent };
-  sparseCellUpdateData?: { [key: string]: string };
-  columns: string[];
+    sparseCellData: any[];
+    sparseCellInvalid?: { [key: string]: Intent };
+    sparseCellUpdateData?: { [key: string]: string };
+    columns: string[];
+    cachedData: any[];
 }
 
 export class VTable extends Component<IProps, IVTableState> {
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  public static dataKey = (rowIndex: number, columnIndex: number) => {
-    return `${rowIndex}-${columnIndex}`;
-  };
-
-  public state: IVTableState = {
-    sparseCellData: this.props.data,
-    sparseCellInvalid: {},
-    sparseCellUpdateData: {},
-    columns: this.props.columns
-  };
-
-  render() {
-    const { sortable } = this.props;
-    const columns = this.state.columns;
-    const columnsList = columns.map((name: string, index: number) => {
-      const col = new TableColumn(name, index, columns, sortable);
-      return col.getColumn(this.renderCell);
-    });
-
-    return (
-      <Table
-        numRows={this.state.sparseCellData.length}
-        onColumnsReordered={this._handleColumnsReordered}
-        enableColumnReordering={this.props.reordering}
-        bodyContextMenuRenderer={this.renderBodyContextMenu}
-      >
-        {columnsList}
-      </Table>
-    );
-  }
-
-  public renderCell = (rowIndex: number, columnIndex: number) => {
-    const dataKey = VTable.dataKey(rowIndex, columnIndex);
-    const { edit } = this.props;
-    const columns = this.state.columns;
-    const data = this.state.sparseCellData;
-    const value = data[rowIndex][columns[columnIndex]];
-    return edit && edit.columns.indexOf(columns[columnIndex]) !== -1 ? (
-      <EditableCell
-        value={value == null ? '' : value}
-        intent={this.state.sparseCellInvalid![dataKey]}
-        onCancel={this.cellValidator(rowIndex, columnIndex)}
-        onChange={this.cellValidator(rowIndex, columnIndex)}
-        onConfirm={this.cellSetter(rowIndex, columnIndex)}
-      />
-    ) : (
-      <Cell>{value}</Cell>
-    );
-  };
-
-  private isValidValue = (columnIndex: number, value: string) => {
-    if (
-      this.props.edit &&
-      this.props.edit.validation &&
-      this.props.edit.validation[this.state.columns[columnIndex]]
-    ) {
-      return this.props.edit.validation[this.state.columns[columnIndex]](value);
+    constructor(props: IProps) {
+        super(props);
     }
-    return true;
-  };
 
-  private cellValidator = (rowIndex: number, columnIndex: number) => {
-    const dataKey = VTable.dataKey(rowIndex, columnIndex);
-    return (value: string) => {
-      if (!this.isValidValue(columnIndex, value)) {
-        this.setSparseCellInvalid(dataKey, Intent.DANGER);
-      } else {
-        this.clearSparseCellInvalid(dataKey);
-      }
-      this.setSparseCellUpdateData(dataKey, value);
-      this.setStateData(rowIndex, columnIndex, value);
+    public static dataKey = (rowIndex: number, columnIndex: number) => {
+        return `${rowIndex}-${columnIndex}`;
     };
-  };
 
-  private cellSetter = (rowIndex: number, columnIndex: number) => {
-    const dataKey = VTable.dataKey(rowIndex, columnIndex);
-    return (value: string) => {
-      if (!this.isValidValue(columnIndex, value)) {
-        this.setSparseCellInvalid(dataKey, Intent.DANGER);
-      } else {
-        this.clearSparseCellInvalid(dataKey);
-      }
-      this.setSparseCellUpdateData(dataKey, value);
-      this.setStateData(rowIndex, columnIndex, value);
+    public state: IVTableState = {
+        sparseCellData: this.props.data,
+        sparseCellInvalid: {},
+        sparseCellUpdateData: {},
+        columns: this.props.columns,
+        cachedData: [],
     };
-  };
 
-  private setSparseCellInvalid = (dataKey: string, value: any) => {
-    this.setState({
-      sparseCellInvalid: {
-        ...this.state.sparseCellInvalid,
-        ...{ [dataKey]: value }
-      }
-    });
-  };
+    render() {
+        const {sortable} = this.props;
+        const columns = this.state.columns;
+        const columnsList = columns.map((name: string, index: number) => {
+            const col = new TableColumn(name, index, columns, sortable);
+            return col.getColumn(this.renderCell);
+        });
 
-  private setSparseCellUpdateData = (dataKey: string, value: any) => {
-    this.setState({
-      sparseCellUpdateData: {
-        ...this.state.sparseCellUpdateData,
-        ...{ [dataKey]: value }
-      }
-    });
-  };
-
-  private clearSparseCellInvalid = (dataKey: string) => {
-    if (this.state.sparseCellInvalid![dataKey]) {
-      const state = this.state;
-      delete state.sparseCellInvalid![dataKey];
-      this.setState(state);
+        return (
+            <Table
+                numRows={this.state.sparseCellData.length}
+                onColumnsReordered={this._handleColumnsReordered}
+                enableColumnReordering={this.props.reordering}
+                bodyContextMenuRenderer={this.renderBodyContextMenu}
+            >
+                {columnsList}
+            </Table>
+        );
     }
-  };
 
-  private setStateData = (
-    rowIndex: number,
-    columnIndex: number,
-    value: string
-  ) => {
-    const data = this.state.sparseCellData;
-    data[rowIndex][this.state.columns[columnIndex]] = value;
-    this.setState({
-      sparseCellData: data
-    });
-  };
+    public renderCell = (rowIndex: number, columnIndex: number) => {
+        const dataKey = VTable.dataKey(rowIndex, columnIndex);
+        const {edit} = this.props;
+        const columns = this.state.columns;
+        const data = this.state.sparseCellData;
+        const value = data[rowIndex][columns[columnIndex]];
+        return edit && edit.columns.indexOf(columns[columnIndex]) !== -1 ? (
+            <EditableCell
+                value={value == null ? '' : value}
+                intent={this.state.sparseCellInvalid![dataKey]}
+                onCancel={this.cellValidator(rowIndex, columnIndex)}
+                onChange={this.cellValidator(rowIndex, columnIndex)}
+                onConfirm={this.cellSetter(rowIndex, columnIndex)}
+            />
+        ) : (
+            <Cell>{value}</Cell>
+        );
+    };
 
-  private renderBodyContextMenu = (context: IMenuContext) => {
-    return this.props.contextual ? (
-      <ActionCellsMenuItem
-        context={context}
-        getCellData={this.getCellData}
-        context_options={this.props.contextual!}
-        onDefaultActions={this.onDefaultActions}
-      />
-    ) : (
-      <div />
-    );
-  };
+    private isValidValue = (columnIndex: number, value: string) => {
+        if (
+            this.props.edit &&
+            this.props.edit.validation &&
+            this.props.edit.validation[this.state.columns[columnIndex]]
+        ) {
+            return this.props.edit.validation[this.state.columns[columnIndex]](value);
+        }
+        return true;
+    };
 
-  private getCellData = (rowIndex: number, columnIndex: number) => {
-    const data = this.state.sparseCellData[rowIndex];
-    return data[this.state.columns[columnIndex]];
-  };
+    private cellValidator = (rowIndex: number, columnIndex: number) => {
+        const dataKey = VTable.dataKey(rowIndex, columnIndex);
+        return (value: string) => {
+            if (!this.isValidValue(columnIndex, value)) {
+                this.setSparseCellInvalid(dataKey, Intent.DANGER);
+            } else {
+                this.clearSparseCellInvalid(dataKey);
+            }
+            this.setSparseCellUpdateData(dataKey, value);
+            this.setStateData(rowIndex, columnIndex, value);
+        };
+    };
 
-  private onDefaultActions(action: DefaultActions, value: any) {
-    switch (action) {
-      case 'copy':
-        console.log(value);
-        break;
-      case 'paste':
-        console.log(value);
-        break;
-      case 'export':
-        console.log(value);
-        break;
-    }
-  }
+    private cellSetter = (rowIndex: number, columnIndex: number) => {
+        const dataKey = VTable.dataKey(rowIndex, columnIndex);
+        return (value: string) => {
+            if (!this.isValidValue(columnIndex, value)) {
+                this.setSparseCellInvalid(dataKey, Intent.DANGER);
+            } else {
+                this.clearSparseCellInvalid(dataKey);
+            }
+            this.setSparseCellUpdateData(dataKey, value);
+            this.setStateData(rowIndex, columnIndex, value);
+        };
+    };
 
-  private _handleColumnsReordered = (
-    oldIndex: number,
-    newIndex: number,
-    length: number
-  ) => {
-    if (oldIndex === newIndex) {
-      return;
-    }
-    const nextChildren = Utils.reorderArray(
-      this.state.columns,
-      oldIndex,
-      newIndex,
-      length
-    );
-    this.setState({ columns: nextChildren });
-  };
+    private setSparseCellInvalid = (dataKey: string, value: any) => {
+        this.setState({
+            sparseCellInvalid: {
+                ...this.state.sparseCellInvalid,
+                ...{[dataKey]: value}
+            }
+        });
+    };
+
+    private setSparseCellUpdateData = (dataKey: string, value: any) => {
+        this.setState({
+            sparseCellUpdateData: {
+                ...this.state.sparseCellUpdateData,
+                ...{[dataKey]: value}
+            }
+        });
+    };
+
+    private clearSparseCellInvalid = (dataKey: string) => {
+        if (this.state.sparseCellInvalid![dataKey]) {
+            const state = this.state;
+            delete state.sparseCellInvalid![dataKey];
+            this.setState(state);
+        }
+    };
+
+    private setStateData = (
+        rowIndex: number,
+        columnIndex: number,
+        value: string
+    ) => {
+        const data = this.state.sparseCellData;
+        data[rowIndex][this.state.columns[columnIndex]] = value;
+        this.setState({
+            sparseCellData: data
+        });
+    };
+
+    setCachedData = (cachedData: any[]) => {
+        this.setState({...this.state, cachedData})
+    };
+
+    cachedData = () => {
+        return this.state.cachedData;
+    };
+
+    private renderBodyContextMenu = (context: IMenuContext) => {
+        return this.props.contextual ? (
+            <ActionCellsMenuItem
+                context={context}
+                getCellData={this.getCellData}
+                context_options={this.props.contextual!}
+                onDefaultActions={this.onDefaultActions}
+                setCachedData={this.setCachedData}
+                cachedData={this.cachedData}
+                setStateData={this.setStateData}
+            />
+        ) : (
+            <div/>
+        );
+    };
+
+    private getCellData = (rowIndex: number, columnIndex: number) => {
+        const data = this.state.sparseCellData[rowIndex];
+        return data[this.state.columns[columnIndex]];
+    };
+
+    private onDefaultActions = (action: DefaultActions, value: any) => {
+        switch (action) {
+            case 'copy':
+                console.log(value);
+                break;
+            case 'paste':
+                console.log(value);
+                break;
+            case 'export':
+                console.log(value);
+                break;
+        }
+    };
+
+    private _handleColumnsReordered = (
+        oldIndex: number,
+        newIndex: number,
+        length: number
+    ) => {
+        if (oldIndex === newIndex) {
+            return;
+        }
+        const nextChildren = Utils.reorderArray(
+            this.state.columns,
+            oldIndex,
+            newIndex,
+            length
+        );
+        this.setState({columns: nextChildren});
+    };
 }
