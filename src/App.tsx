d037620ba06625511667c833e817b664c69cdf17
gsi-arturo <arturo@generalsofwareinc.com>
2019-02-19 11:21:14 -0500

    Added VCardPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,84 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
-import { VTable } from "./components/Table/Table";
+import {VTable} from "./components/Table/Table";
+import {VCardPanel} from './components/Card/VCardPanel';
 
 class App extends Component {
-  render() {
-    const data = [
-      { name: "Carlos", lastname: "Chao" },
-      { name: "Name1", lastname: "Lastname1" },
-      { name: "Name2", lastname: "Lastname2" },
-      { name: "Name3", lastname: "Lastname3" },
-      { name: "Name4", lastname: "Lastname4" },
-      { name: "Name5", lastname: "Lastname5" },
-      { name: "Name6", lastname: "Lastname6" },
-      { name: "Name7", lastname: "Lastname7" },
-      { name: "Name7", lastname: "Lastname7" },
-      { name: "Name7", lastname: "Lastname7" },
-      { name: "Name7", lastname: "Lastname7" },
-      { name: "Name7", lastname: "Lastname7" },
-    ];
+    render() {
+        const data = [
+            {name: "Carlos", lastname: "Chao"},
+            {name: "Name1", lastname: "Lastname1"},
+            {name: "Name2", lastname: "Lastname2"},
+            {name: "Name3", lastname: "Lastname3"},
+            {name: "Name4", lastname: "Lastname4"},
+            {name: "Name5", lastname: "Lastname5"},
+            {name: "Name6", lastname: "Lastname6"},
+            {name: "Name7", lastname: "Lastname7"},
+            {name: "Name7", lastname: "Lastname7"},
+            {name: "Name7", lastname: "Lastname7"},
+            {name: "Name7", lastname: "Lastname7"},
+            {name: "Name7", lastname: "Lastname7"},
+        ];
 
-    // validator example
-    const nameValidation = (value: string) => {
-      return value.length > 5;
-    };
+        // validator example
+        const nameValidation = (value: string) => {
+            return value.length > 5;
+        };
 
-    return (
-      <VTable
-          edit={{ columns: ["name"], validation: {name: nameValidation} }}
-          columns={["name", "lastname"]}
-          reordering={true}
-          sortable={{columns: ["name"], onSort: this.onSort}}
-          contextual={{columns: ["name"], default_actions: ["copy", "paste", "export"], actions: [{
-            icon: 'export', action: (item: any) => console.log(item), text: "Action Input"
-              }]}}
-          data={data}
-      />
-    );
-  }
+        return (
+            <div style={{
+                padding: '20px',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+            }}>
+                <VCardPanel headerText={'This is the header of the card'}
+                       headerIcon={'calendar'}
+                       headerBackgroundColor={'violet'}
+                       headerColor={'white'}
+                       cardElevation={3}
+                       headerHorizontalAlign={'start'}
+                       height={'350px'}
+                       width={'30%'}
+                >
+                    <div>This is the card content</div>
+                </VCardPanel>
+                <VCardPanel collapse
+                       keepChildrenMounted
+                       headerTextJustify={'center'}
+                       bodyPadding={'10px'}
+                       openIcon={'plus'}
+                       closeIcon={'minus'}
+                       headerText={'This is the header of the 2 card'}
+                       cardElevation={2}
+                       height={'150px'}
+                       width={'25%'}
+                >
+                    <div>This is the card content</div>
+                </VCardPanel>
+                <VCardPanel headerIcon={'calendar'}
+                       headerBackgroundColor={'blue'}
+                       headerColor={'white'}
+                       cardElevation={3}
+                       headerHorizontalAlign={'center'}
+                       height={'140px'}
+                       width={'15%'}
+                >
+                    <div>This is the card content of a card with no text in the header</div>
+                </VCardPanel>
 
-  onSort = (index: number, order: string) => {
-    console.log(index);
-    console.log(order);
-  }
+                <VCardPanel headerText={'This is the header of the  default card'}
+                >
+                    <div>This is the card content</div>
+                </VCardPanel>
+            </div>
+        );
+    }
+
+    onSort = (index: number, order: string) => {
+        console.log(index);
+        console.log(order);
+    }
 }
 
 export default App;
---
+++ src/components/Card/VCardPanel.tsx
@@ -0,0 +1,110 @@
+import {Elevation, Collapse, IconName, Icon} from "@blueprintjs/core";
+import '@blueprintjs/core/lib/css/blueprint.css';
+import {VCardHeader, VCardBody, VCardTextSpan, VCard, VCardTextSpanContainer} from './style';
+import React, {Component} from 'react';
+
+interface StyledCardProps {
+    noHeader?: boolean;
+    headerText?: string;
+    children?: any;
+    headerIcon?: any
+    headerBackgroundColor?: any;
+    backgroundColor?: any;
+    headerColor?: any;
+    headerHorizontalAlign?: FlexJustify;
+    cardElevation?: Elevation;
+    height?: string;
+    width?: string;
+    collapse?: boolean;
+    transitionDuration?: number;
+    bodyPadding?: string;
+    keepChildrenMounted?: boolean;
+    openIcon?: IconName;
+    closeIcon?: IconName;
+    headerOrientation?: HeaderOrientation
+    headerTextJustify?: FlexJustify
+}
+
+interface PanelState {
+    isOpen: boolean;
+}
+
+export type FlexJustify = 'center' | 'start' | 'end';
+export type HeaderOrientation = 'row' | 'row-reverse'
+export class VCardPanel extends Component<StyledCardProps, PanelState> {
+    constructor(props: StyledCardProps) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        }
+    }
+
+    toggleCollapsed = () => {
+        const {isOpen} = this.state;
+        this.setState({...this.state, isOpen: !isOpen})
+    };
+
+    render() {
+        const {
+            headerIcon,
+            headerText,
+            children,
+            headerBackgroundColor,
+            headerHorizontalAlign,
+            headerColor,
+            backgroundColor,
+            cardElevation,
+            height,
+            width,
+            collapse,
+            transitionDuration,
+            bodyPadding,
+            keepChildrenMounted,
+            closeIcon,
+            openIcon,
+            headerOrientation,
+            headerTextJustify,
+            noHeader
+
+        } = this.props;
+        const {isOpen} = this.state;
+        return (
+            <VCard elevation={cardElevation || 0}
+                   height={height}
+                   collapse={collapse}
+                   transitionDuration={transitionDuration}
+                   isOpen={isOpen}
+                   width={width}>
+                {!noHeader?
+                    <VCardHeader headerBackgroundColor={headerBackgroundColor}
+                                headerOrientation = {headerOrientation}
+                                headerJustifyContent={headerHorizontalAlign}>
+                        <VCardTextSpanContainer>
+                            <VCardTextSpan headerColor={headerColor}
+                                          headerTextJustify={headerTextJustify}>
+                                {headerIcon ? <Icon icon={headerIcon}/> : null}
+                                <h5>{headerText}</h5>
+                            </VCardTextSpan>
+                        </VCardTextSpanContainer>
+                        {collapse ?
+                            <span onClick={this.toggleCollapsed}>
+                            <Icon icon={isOpen ? closeIcon || 'chevron-up' : openIcon || 'chevron-down'}/>
+                        </span> : null}
+                    </VCardHeader>
+                    : null}
+                <VCardBody bodyPadding={bodyPadding}
+                          backgroundColor={backgroundColor}>
+                    {collapse ?
+                        <Collapse transitionDuration={transitionDuration || 200}
+                                  keepChildrenMounted={keepChildrenMounted}
+                                  isOpen={isOpen}>
+                            {children}
+                        </Collapse>
+                        :
+                        children
+                    }
+                </VCardBody>
+            </VCard>
+        );
+    }
+}
