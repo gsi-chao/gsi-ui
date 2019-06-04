6462be1db95b1247082a0811a01c15ae01250d80
gsi-johan <johan@generalsoftwareinc.com>
2019-06-03 17:40:04 -0700

    feat: added customer disable contextual menu in table

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
 const TestComponent = () => {
   return <span>Test Component</span>;
