5d163ed4046266759f9903ec76ef50c6012fdd6c
gsi-johan <johan@generalsoftwareinc.com>
2019-06-07 14:41:12 -0700

    feat: migrate vtab component to hooks

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import VTableWithWidgetDoc from './docs/components/Sample';
 
 const TestComponent = () => {
   return <span>Test Component</span>;
 };
 
-ReactDOM.render(<VTableWithWidgetDoc />, document.getElementById('root'));
+ReactDOM.render(<TestComponent/>, document.getElementById('root'));
