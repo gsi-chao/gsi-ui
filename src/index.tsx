41656b0147ae965cb965e9958b365b4142671acf
gsi-arturo <arturo@generalsoftwareinc.com>
2019-08-13 11:39:17 -0700

    Feat -Added option to set all text in upperCase to the inputs and the text Areas

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import { VInputField, VTextAreaField } from './components/Form';
-import { FieldState } from 'formstate';
 
 const TestComponent = () => {
-  return <div>
-    <VInputField id={'id'} fieldState={new FieldState<any>('initial value')} upperCaseFormat/>
-    <VTextAreaField id={'id2'} fieldState={new FieldState<any>('initial value')} upperCaseFormat/>
-  </div>;
+  return <div>Test Component!!</div>;
 };
 
 ReactDOM.render(<TestComponent />, document.getElementById('root'));
