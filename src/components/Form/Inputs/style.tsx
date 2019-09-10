8fa5655f14cb863e192360d6b0a65164f21cc217
gsi-arturo <arturo@generalsoftwareinc.com>
2019-09-10 16:52:35 -0700

    Feat - Added tip label option to numeric field component

--- src/components/Form/Inputs/style.tsx
+++ src/components/Form/Inputs/style.tsx
@@ -30,60 +30,60 @@ export const layerInPercent = (layer: ILayer): any => {
 
 export const StyledFormGroup = styled(FormGroup)`
   margin: ${(props: IStyledFieldProps) =>
-  props.margin ? props.margin : '0 0 15px'};
+    props.margin ? props.margin : '0 0 15px'};
   ${(props: IStyledFieldProps) => {
-  const { layer, noLabel, checkBoxAtLeft, inline, fill } = props;
-  let layerPercent: any = {};
-  let inputOrientation = 'flex-start';
-  let containerWidth = undefined;
-  let labelWidth = undefined;
-  let labelOrientation = undefined;
-  let inputWidth = undefined;
-  if (layer) {
-    layerPercent = layer ? layerInPercent(layer) : undefined;
-    inputOrientation =
-      layer.inputOrientation === 'center'
-        ? 'center'
-        : layer.inputOrientation === 'end'
-        ? 'flex-end'
-        : 'flex-start';
-    labelOrientation = layer.labelOrientation;
-    if (layerPercent) {
-      containerWidth = layerPercent.containerWidth;
-      labelWidth = layerPercent.labelWidth;
-      inputWidth = layerPercent.inputWidth;
+    const { layer, noLabel, checkBoxAtLeft, inline, fill } = props;
+    let layerPercent: any = {};
+    let inputOrientation = 'flex-start';
+    let containerWidth = undefined;
+    let labelWidth = undefined;
+    let labelOrientation = undefined;
+    let inputWidth = undefined;
+    if (layer) {
+      layerPercent = layer ? layerInPercent(layer) : undefined;
+      inputOrientation =
+        layer.inputOrientation === 'center'
+          ? 'center'
+          : layer.inputOrientation === 'end'
+          ? 'flex-end'
+          : 'flex-start';
+      labelOrientation = layer.labelOrientation;
+      if (layerPercent) {
+        containerWidth = layerPercent.containerWidth;
+        labelWidth = layerPercent.labelWidth;
+        inputWidth = layerPercent.inputWidth;
+      }
     }
-  }
-  return `
+    return `
         & .bp3-form-content {
             display: flex;
             flex-direction:${
-    inline
-      ? `row${checkBoxAtLeft ? `-reverse` : ''};
+              inline
+                ? `row${checkBoxAtLeft ? `-reverse` : ''};
             align-content: center;`
-      : 'column'
-    }
+                : 'column'
+            }
             ${
-    containerWidth
-      ? `width: ${containerWidth}%!important;`
-      : 'width: 100%!important;'
-    }
+              containerWidth
+                ? `width: ${containerWidth}%!important;`
+                : 'width: 100%!important;'
+            }
             & label.field-label {
                 line-height: 30px;
                     ${labelWidth ? `width: ${labelWidth}%!important;` : ''}
                     ${
-    labelOrientation ? `text-align: ${labelOrientation}` : ''
-    }    
+                      labelOrientation ? `text-align: ${labelOrientation}` : ''
+                    }    
             }
             & .gsi-form-field-container {
                 ${!noLabel ? `padding-left: 5px!important;` : ''}
                 width: ${
-    inputWidth
-      ? `${inputWidth}%`
-      : inputOrientation === 'flex-end'
-      ? `calc(${100 - labelWidth}% - 38px)`
-      : `${100 - labelWidth}%`
-    }!important;
+                  inputWidth
+                    ? `${inputWidth}%`
+                    : inputOrientation === 'flex-end'
+                    ? `calc(${100 - labelWidth}% - 38px)`
+                    : `${100 - labelWidth}%`
+                }!important;
                 display: flex;
                 align-items:${inputOrientation};
                 flex-direction: column;
@@ -91,14 +91,14 @@ export const StyledFormGroup = styled(FormGroup)`
                   display: flex;
                   flex-direction: column;
                 ${
-    fill
-      ? `width: ${100}%;
+                  fill
+                    ? `width: ${100}%;
                 & .bp3-popover-target {
                   width: 100%;
                 }
                 `
-      : `max-width: 200px;`
-    };
+                    : `max-width: 200px;`
+                };
                 & .gsi-error-span {
                     padding-top: 1px;
                     font-size: 12px;
@@ -108,7 +108,7 @@ export const StyledFormGroup = styled(FormGroup)`
             }
         }          
 `;
-}}
+  }}
 `;
 export const StyledInput = styled(StyledFormGroup)`
   .gsi-input-and-error-container {
@@ -143,6 +143,15 @@ export const StyledSelect = styled(StyledFormGroup)`
 
 export const StyledNumericInput = styled(StyledFormGroup)`
   .gsi-input-and-error-container {
+    & span.tipLabel {
+      background: white;
+      margin-bottom: -7px;
+      z-index: 1;
+      width: fit-content;
+      font-weight: 400;
+      font-size: 12px;
+      padding: 0 2px;
+    }
     & .bp3-control-group.bp3-numeric-input {
       width: 100%;
       & .bp3-input-group {
@@ -155,38 +164,38 @@ export const StyledNumericInput = styled(StyledFormGroup)`
 export const StyledCheckBoxInput = styled(StyledFormGroup)`
   label.field-label {
     padding-left: ${(props: IStyledFieldProps) =>
-  props.checkBoxAtLeft &&
-  props.layer &&
-  (props.layer.labelOrientation === 'start' ||
-    !props.layer.labelOrientation)
-    ? 12
-    : 0}px!important;
+      props.checkBoxAtLeft &&
+      props.layer &&
+      (props.layer.labelOrientation === 'start' ||
+        !props.layer.labelOrientation)
+        ? 12
+        : 0}px!important;
   }
   .gsi-form-field-container {
     & .gsi-input-and-error-container {
       & .bp3-control.bp3-checkbox,
       .bp3-inline.bp3-align-right {
         ${(props: IStyledFieldProps) =>
-  props.checkBoxAtLeft ? '' : `padding: 0!important;`};
+          props.checkBoxAtLeft ? '' : `padding: 0!important;`};
         width: 0 !important;
         text-align: left;
         margin-right: 0 !important;
         ${(props: IStyledFieldProps) => {
-  const inputOrientation =
-    props.layer && props.layer.inputOrientation === 'center'
-      ? 'center'
-      : props.layer && props.layer.inputOrientation === 'end'
-      ? 'flex-end'
-      : 'flex-start';
-  return `align-self: ${inputOrientation};`;
-}};
+          const inputOrientation =
+            props.layer && props.layer.inputOrientation === 'center'
+              ? 'center'
+              : props.layer && props.layer.inputOrientation === 'end'
+              ? 'flex-end'
+              : 'flex-start';
+          return `align-self: ${inputOrientation};`;
+        }};
         & span.bp3-control-indicator {
           margin-left: ${(props: IStyledFieldProps) =>
-  props.checkBoxAtLeft &&
-  props.layer &&
-  props.layer.inputOrientation === 'end'
-    ? 9
-    : 0}px !important;
+            props.checkBoxAtLeft &&
+            props.layer &&
+            props.layer.inputOrientation === 'end'
+              ? 9
+              : 0}px !important;
           float: left;
         }
       }
@@ -214,9 +223,9 @@ export const StyledSlider = styled(StyledFormGroup)`
   .gsi-input-and-error-container {
     padding-left: 9px !important;
     ${(props: IStyledFieldProps) =>
-  props.fill
-    ? `width: calc(100% - 21px)!important;`
-    : `max-width: 200px!important;`};
+      props.fill
+        ? `width: calc(100% - 21px)!important;`
+        : `max-width: 200px!important;`};
     & .bp3-slider {
       width: 100%;
     }
@@ -226,7 +235,7 @@ export const StyledSlider = styled(StyledFormGroup)`
 export const StyledPopOverWrapper = styled(StyledFormGroup)`
   .gsi-input-and-error-container {
     ${(props: IStyledFieldProps) =>
-  props.fill ? `width: 100%!important;` : `max-width: 200px!important;`};
+      props.fill ? `width: 100%!important;` : `max-width: 200px!important;`};
     & span.tipLabel {
       margin-bottom: -7px;
       z-index: 1;
@@ -242,9 +251,9 @@ export const StyledPopOverWrapper = styled(StyledFormGroup)`
         div {
           & > button:not(.crossButton) {
             width: ${(props: IStyledFieldProps) =>
-  props.fixedInputWidthPx
-    ? `${props.fixedInputWidthPx}px`
-    : `100%`};
+              props.fixedInputWidthPx
+                ? `${props.fixedInputWidthPx}px`
+                : `100%`};
             display: flex;
             justify-content: space-between;
             &:focus {
@@ -273,14 +282,14 @@ export const StyledRadioButton = styled(StyledFormGroup)`
           top: -5px;
           display: flex;
           ${(props: IStyledFieldProps) => {
-  const inputOrientation =
-    props.layer && props.layer.inputOrientation === 'center'
-      ? 'center'
-      : props.layer.inputOrientation === 'end'
-      ? 'flex-end'
-      : 'flex-start';
-  return `justify-content: ${inputOrientation};`;
-}};
+            const inputOrientation =
+              props.layer && props.layer.inputOrientation === 'center'
+                ? 'center'
+                : props.layer.inputOrientation === 'end'
+                ? 'flex-end'
+                : 'flex-start';
+            return `justify-content: ${inputOrientation};`;
+          }};
           & .bp3-control.bp3-radio.bp3-inline {
             padding: 0 26px !important;
             width: auto !important;
@@ -304,7 +313,7 @@ export const StyledRadioButton = styled(StyledFormGroup)`
 export const IconDate = styled('div')`
   display: flex;
   background-color: ${(props: IIconStyle) =>
-  props.backgroundColor ? props.backgroundColor : '#dcdcdc'};
+    props.backgroundColor ? props.backgroundColor : '#dcdcdc'};
   width: 30px;
   height: 30px;
   justify-content: center;
@@ -335,10 +344,9 @@ export const DateInputPicker = styled(DatePicker)`
 `;
 
 export const DateInputContainer = styled(DateInput)`
-@-moz-document url-prefix() {
-   & .bp3-html-select.bp3-minimal select{
-       font-size: 12px;
+  @-moz-document url-prefix() {
+    & .bp3-html-select.bp3-minimal select {
+      font-size: 12px;
     }
-}
+  }
 `;
-
