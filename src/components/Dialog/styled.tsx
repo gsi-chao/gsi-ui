7a3e575a2644bdc90556ba087780c918cf430bf4
Rafael <rafaelmayor22@gmail.com>
2019-09-13 10:39:59 +0200

    hotfix in dimension of the VDraggableDialog

--- src/components/Dialog/styled.tsx
+++ src/components/Dialog/styled.tsx
@@ -1,11 +1,28 @@
 import styled from 'styled-components';
 import { Dialog } from '@blueprintjs/core';
+import { Rnd } from 'react-rnd';
+
+interface IDialogProps {
+  width?: number;
+  height?: number;
+}
 
 export const DialogStyled = styled(Dialog)`
-  margin: 0px;
-   & > .bp3-dialog-container.bp3-overlay-content.bp3-overlay-enter-done:focus {
+    margin: 0px;
+    width: ${(props: IDialogProps) => `${props.width}px!important`};
+    height: ${(props: IDialogProps) => `${props.height}px!important`};
+        
+  & > .bp3-dialog-container.bp3-overlay-content.bp3-overlay-enter-done:focus {
       outline: rgba(255, 255, 255, 0) auto 2px;
       outline-offset: 2px;
       -moz-outline-radius: 6px;
   }
+
+`;
+
+export const RndStyled = styled(Rnd)`
+ &> .bp3-overlay.bp3-overlay-open.bp3-overlay-inline.bp3-overlay-scroll-container{
+      width: ${(props: IDialogProps) => `${props.width}px!important`};
+      height: ${(props: IDialogProps) => `${props.height}px!important`};
+    }
 `;
