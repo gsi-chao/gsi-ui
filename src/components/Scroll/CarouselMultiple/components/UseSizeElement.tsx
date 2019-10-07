41d02c8ac0f0ebc4b79a6eb992f21591e216b175
ovi <ovmelo0303@gmail.com>
2019-10-07 08:03:12 -0400

    add responsive to tabs

---
+++ src/components/Scroll/CarouselMultiple/components/UseSizeElement.tsx
@@ -0,0 +1,14 @@
+import { useState, useRef, useEffect } from 'react'
+
+const useSizeElement = () => {
+  const elementRef = useRef<any>(null);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(elementRef.current && elementRef.current.clientWidth);
+  }, [elementRef.current]);
+
+  return { width, elementRef };
+}
+
+export default useSizeElement;
---
+++ src/components/Scroll/CarouselMultiple/style.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+interface ICarouselContainer {
+  sliding:  boolean,
+  direction:  string,
+}
+
+export const SliderWrapper = styled.div`    
+      width: 100%;
+      padding: 40px 0 0;
+      overflow: hidden;
+      position: relative;
+      .slider {
+        display: flex;
+        position: relative;
+    
+        &__container {
+          display: flex;
+          padding: 0;
+          transition: transform 300ms ease 100ms;
+          z-index: 3;
+          width: 100%;
+        }
+        
+        &:not(&--open):hover .item {
+          transform: translateX(-25%);
+        }
+      
+        &:not(&--open) .item:hover ~ .item {
+          transform: translateX(25%);
+        }  
+      }  
+      .item {
+        //flex: 0 0 19.7%;
+        transition: transform 300ms ease 100ms;
+        margin: 0 2px;
+        position: relative;
+      }
+`;
+
+export const SliderButtonSC = styled.button`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 25px;
+  background: rgba(0, 0, 0, 0.5);
+  border: 0;
+  outline: 0;
+  padding: 0;
+  margin: 40px 0 0;
+  z-index: 4;
+
+  span {
+    width: 25px;
+    color: #fff;
+    display: block;
+    margin: 0 auto;
+  }
+
+  &.slide-button--next {
+    right: 0;
+  }
+
+  &.slide-button--prev {
+    left: 0;
+  }
+`;
