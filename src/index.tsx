98f0aedd79ab1a96d3c0672110396ebe9919b9be
gsi-arturo <arturo@generalsoftwareinc.com>
2019-11-14 14:35:20 -0800

    Feat: Fixed Select not displaying the clear selection option when initializing

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,9 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import { VColorPicker } from './components/ColorPicker';
-const TestComponent = () => {
-  const [color, setColor] = useState<any>('#fff');
 
-  const onChange = (color: any) => {
-    setColor(color.hex);
-  };
-  return (
-    <div>
-      <VColorPicker
-        Color={color}
-        onChange={onChange}
-        typePickerColor={'ChromePicker'}
-        addButton={{
-          text: 'add',
-          intent: 'success'
-        }}
-      />
-    </div>
-  );
+const TestComponent = () => {
+  return <div>Test Component!</div>;
 };
 
 ReactDOM.render(<TestComponent />, document.getElementById('root'));
