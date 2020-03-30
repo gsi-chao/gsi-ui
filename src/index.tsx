dea8874f5d7000ea49c21b7726994bbef45ce7dd
gsi-rafaelmayor <rafael.mayor@generalsoftwareinc.com>
2020-03-30 15:26:24 -0500

    Feat: Add ability of create new element in SelectField component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
 const TestComponent = observer(() => {
 
