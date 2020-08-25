cc763c855042f37acff2bc8500c45602633c21ea
gsi-rafaelmayor <rafael.mayor@generalsoftwareinc.com>
2020-08-25 13:29:39 -0500

    fix(search_select): add the ability to display the label when selecting an item

--- src/components/Form/Inputs/SearchSelect/SearchSelect.tsx
+++ src/components/Form/Inputs/SearchSelect/SearchSelect.tsx
@@ -61,8 +61,14 @@ export const SearchSelect = (props: IProps) => {
       const option = findOptionsValue(props.options, props.value);
       option && option.label && setSearch(option.label.toString());
     } else {
-      setSearch('');
+      if (props.multi && props.value.length === 1) {
+        const option = findOptionsValue(props.options, props.value[0]);
+        viewSelection(option);
+      } else {
+        clearSearch();
+      }
     }
+
     setSelection(props.value);
   }, [props.value, props.options]);
 
@@ -105,6 +111,10 @@ export const SearchSelect = (props: IProps) => {
     setOptions(opt);
     !props.popoverWidth && setPopoverWidth(w + 30);
   }, [props.options, search]);
+
+  const clearSearch = () => {
+    setSearch('');
+  };
 
   const onSearchChange = (e: any) => {
     !isOpen && setIsOpen(true);
@@ -149,11 +159,28 @@ export const SearchSelect = (props: IProps) => {
       }
       !props.disabled && setIsOpen(nextOpenState);
       !nextOpenState && onChangeItems(selection);
-      props.multi && setSearch('');
+      props.multi && resetStateMulti();
       !props.multi && selection && setSearchSelectionText(selection || '');
-      !props.multi && options.length === 0 && setSearch('');
+      !props.multi && options.length === 0 && clearSearch();
     } catch (e) {
       !props.disabled && setIsOpen(nextOpenState);
+    }
+  };
+
+  const resetStateMulti = () => {
+    if (props.multi && selection?.length === 1 && !!props?.value?.length) {
+      const { options } = props;
+      const elementSelect = find(
+        options,
+        (item: IItem) => item.value === props.value[0]
+      );
+      const elementSelectLabel = elementSelect?.label ?? '';
+      if (elementSelectLabel !== search) {
+        setSearch(elementSelectLabel);
+        setEnableFilter(false);
+      }
+    } else if (props.multi && !props?.value?.length && !selection.length) {
+      clearSearch();
     }
   };
 
@@ -176,7 +203,12 @@ export const SearchSelect = (props: IProps) => {
       } else {
         selected.push(value.value);
       }
-
+      setEnableFilter(false);
+      clearSearch();
+      if (selected.length === 1) {
+        const option = findOptionsValue(props.options, selected[0]);
+        viewSelection(option);
+      }
       setSelection(selected);
     } else {
       setSelection(value.value);
@@ -192,6 +224,14 @@ export const SearchSelect = (props: IProps) => {
       return selection.length;
     }
     return 0;
+  };
+
+  const viewSelection = (value: IItem) => {
+    if (value.label) {
+      setSearch(value.label.toString());
+    } else {
+      clearSearch();
+    }
   };
 
   const onKeyPress = (e: React.KeyboardEvent<HTMLElement>) => {
@@ -212,7 +252,7 @@ export const SearchSelect = (props: IProps) => {
   const onAddNewItem = () => {
     props.onAddNewItem && props.onAddNewItem(search);
     setIsOpen(false);
-    setSearch('');
+    clearSearch();
   };
 
   return (
