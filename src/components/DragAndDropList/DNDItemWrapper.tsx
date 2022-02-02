c97e46a0d8660e0558d9aa889e73da967eb7376c
gsi-rafaelmayor <rafael.mayor@generalsoftwareinc.com>
2022-02-02 12:52:51 -0500

    feat(draganddroplist): add the ability to disable a list

--- src/components/DragAndDropList/DNDItemWrapper.tsx
+++ src/components/DragAndDropList/DNDItemWrapper.tsx
@@ -1,6 +1,6 @@
 import { CustomDraggableItem } from './CustomDraggableItem';
 import React, { CSSProperties } from 'react';
-import { DNDItem } from './style';
+import { DNDItem, DNDItemBody } from './style';
 import { DraggableProvided } from 'react-beautiful-dnd';
 import { IDNDItem, IDNDList, SelectedItemHelpButtonList } from './types';
 import { useClickPreventionOnDoubleClick } from '../CustomHooks';
@@ -17,6 +17,7 @@ export interface DNDItemWrapperProps {
     destination: any
   ) => void;
   displayButtons?: boolean;
+  disabled?: boolean;
   selectedItemHelpButtons?: SelectedItemHelpButtonList;
   value: IDNDList;
   innerRef: any;
@@ -32,34 +33,38 @@ export const DNDItemWrapper = (props: DNDItemWrapperProps) => {
     handleHelpButtonClicked,
     displayButtons,
     value,
-    selectedItemHelpButtons
+    selectedItemHelpButtons,
+    disabled
   } = props;
   const [handleClick, handleDoubleClick] = useClickPreventionOnDoubleClick(
     onClick,
     onDoubleClick
   );
   return (
-    <DNDItem
-      onClick={handleClick}
-      onDoubleClick={handleDoubleClick}
-      className={className}
-      ref={props.innerRef}
-      {...providedDraggable.draggableProps}
-      {...providedDraggable.dragHandleProps}
-      style={{
-        ...((providedDraggable.draggableProps.style || {}) as CSSProperties)
-      }}
-    >
-      <CustomDraggableItem
-        label={item.label}
-        type={item.type || ''}
-        source={value.id}
-        displayButtons={displayButtons}
-        selectedItemHelpButtons={selectedItemHelpButtons}
-        draggableId={item.value}
-        handleHelpButtonClicked={handleHelpButtonClicked}
-      />
-      {providedDraggable.placeholder}
-    </DNDItem>
+    <DNDItemBody disabled={disabled}>
+      <DNDItem
+        onClick={handleClick}
+        onDoubleClick={handleDoubleClick}
+        className={className}
+        disabled={disabled}
+        ref={props.innerRef}
+        {...providedDraggable.draggableProps}
+        {...providedDraggable.dragHandleProps}
+        style={{
+          ...((providedDraggable.draggableProps.style || {}) as CSSProperties)
+        }}
+      >
+        <CustomDraggableItem
+          label={item.label}
+          type={item.type || ''}
+          source={value.id}
+          displayButtons={displayButtons}
+          selectedItemHelpButtons={selectedItemHelpButtons}
+          draggableId={item.value}
+          handleHelpButtonClicked={handleHelpButtonClicked}
+        />
+        {providedDraggable.placeholder}
+      </DNDItem>
+    </DNDItemBody>
   );
 };
